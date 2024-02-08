8a6947560a6de9cdb83df998cf7d447a3aa68f9c
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2024-02-08 15:12:32 -0500

    adds search directory button and seach functionality to Search page

--- react/src/components/Search.jsx
+++ react/src/components/Search.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
@@ -6,19 +5,36 @@ const url = new URL("http://localhost:4000/search");
 
 function Search() {
   const [employees, setEmployees] = useState([]);
-  const getEmployees = function async() {
-    fetch(url)
+  const [searchInput, setSearchInput] = useState("");
+
+  const getEmployees = function async(searchInput = '') {
+    console.log()
+    fetch(url + searchInput)
       .then(async (response) => await response.json())
       .then((data) => {
         setEmployees(data);
       });
   };
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
   useEffect(() => {
     getEmployees();
   }, []);
 
   return (
     <div className="page">
+      <input
+        type="text"
+        placeholder="Enter employee name"
+        onChange={handleInputChange}
+        value={searchInput}
+      />
+      <button onClick={() => getEmployees('/' + searchInput)}>Search Directory</button>
+      
       {employees.map((employee) => {
         return (
           <Link key={employee.id} to={`/profile/${employee.id}`}>
